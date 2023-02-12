992e6c9703e3713c47ef0c6e4b9c79d767ba84e0
Nilson Santiago <nilson.santiago19@gmail.com>
2023-02-11 23:40:11 -0300

    Conclusão de mais dois exercícios

---
+++ pairNumbers.js
@@ -0,0 +1,7 @@
+let c = 1
+while(c <= 100) {
+    if(c % 2 === 0) {
+        console.log(c)
+    }
+    c++
+}
---
+++ positiveNumbers.js
@@ -0,0 +1,19 @@
+const readline = require('readline-sync')
+
+let value = []
+
+// for(let c = 0; c < 6; c++) {
+//     value.push(Number.parseInt(readline.question("Informe um valor positivo ou negativo: ")))
+// }
+
+let quantityPositiveNumbers = value.reduce((acc, number) => {
+    if(number > 0) {
+        return acc + 1
+    }
+    return acc
+}, 0)
+
+// console.log(`${quantityPositiveNumbers} valores positivos`)
+
+let teste = readline.question("Informe um valor positivo ou negativo: ")
+console.log(teste)
